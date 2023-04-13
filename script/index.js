6103d7fca5d95a3b65d66dca47b91c98b6a34d89
Matheus Moreno <111401512+matheusmorenocf@users.noreply.github.com>
2023-04-13 12:48:28 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,24 +1,30 @@
 const buttonSearch = document.querySelector('#button-search');
 const inputSearch = document.querySelector('#input-search');
 
+document.addEventListener('DOMContentLoaded', ipUser)
+
 buttonSearch.addEventListener('click', APIgeo);
 inputSearch.addEventListener('input', formatIP);
 
+
+
+
 // Funcao para permitir somente numeros e pontos no input
-function formatIP() {
+function formatIP(ev) {
+  ev.preventDefault()
   inputSearch.value = inputSearch.value.replace(/[^0-9.]/g, '')                            
 }
 
 // Funçao para requisitar a API os dados do IP informado
-async function APIgeo() {
+async function APIgeo(ev) {
+  
   try {
     const ip = inputSearch.value;
     const response = await fetch (`https://geo.ipify.org/api/v2/country,city?apiKey=at_Oq9bOriVzmnmLqjrpYiNbXflfSmpZ&ipAddress=${ip}`).then(resp => resp.json());
     console.log(response)
     updateOutput(response)
   } catch (error) {
-    console.log(error)
-    alert('Endereço de IP inválido')
+    alert(error)
   }
 }
 
@@ -45,3 +51,19 @@ function updateOutput(data) {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 }
+
+
+async function ipUser() {
+  fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const ipAddress = data.ip;
+    
+    inputSearch.value = ipAddress
+  })
+  .catch(error => {
+    console.error('Erro ao obter o endereço IP:', error);
+  });
+
+}
+
